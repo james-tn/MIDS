3139f5416b3e6432321ea25489e27410066d3479
James Nguyen <james.n@ischool.berkeley.edu>
2017-06-24 21:48:25 -0700

    commit

---
+++ w205/exercise_1/hive_base_ddl.sql
@@ -0,0 +1,6 @@
+DROP TABLE HOSPITALS;
+
+CREATE EXTERNAL TABLE HOSPITALS (Provider_ID string, Hospital_Name string, Address string, City string, State string, Zip_Code string, County_Name string, Phone_Number string, Hospital_Type string, Hospital_Ownership string) ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( "separatorChar" = ",",  "quoteChar" = '"',  "escapeChar" = '\\' ) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/hospital_compare/hospital.csv’; 
