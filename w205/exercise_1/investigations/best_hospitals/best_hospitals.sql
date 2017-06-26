4cbe43bdaa09d79f435c9f83891af0c924e63654
James Nguyen <james.n@ischool.berkeley.edu>
2017-06-26 11:37:41 -0700

    commit
    commit

--- w205/exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ w205/exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -1,2 +1,2 @@
-select * from (select format_number(avg(score),1) avg_score,format_number(stddev_pop(score),2) stddev, format_number(sum(score),1) agg_score, count(score) count_score, hospital_name from readmissions_transformed a, hospitals_transformed b where a.provider_id = b.provider_id group by hospital_name having sum(score) > 0 and count(score) >25) t order by avg_score asc, stddev desc  limit 20;
+select * from (select format_number(avg(score),1) avg_score,format_number(stddev_pop(score),2) stddev, format_number(sum(score),1) agg_score, count(score) count_score, hospital_name from readmissions_transformed a, hospitals_transformed b where a.provider_id = b.provider_id group by hospital_name count(score) >25) t order by avg_score asc, stddev desc  limit 15;
 
