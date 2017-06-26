d16633756f5052de60bea5edc6615d541e140013
James Nguyen <james.n@ischool.berkeley.edu>
2017-06-26 12:03:38 -0700

    commit

--- w205/exercise_1/investigations/hospital_variability/hospital_variability.sql
+++ w205/exercise_1/investigations/hospital_variability/hospital_variability.sql
@@ -1,5 +1,3 @@
-select format_number(stddev_samp(score),2) stddev, measure_name from readmissions_transformed a, measures_transformed b where  a.measure_id = b.measure_id group by measure_name order by stddev_pop(score) desc limit 10;
-
 
 select stddev_samp(score), measure_name from effective_care_transformed a, measures_transformed b where  a.measure_id = b.measure_id and b.measure_name!='Emergency Department Volume' group by measure_name order by stddev_pop(score) desc limit 10;
 
