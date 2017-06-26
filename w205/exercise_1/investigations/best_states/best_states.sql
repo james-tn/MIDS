6ee81313aba8190746a0433eb5aaac9363302843
James-tn <james.n@ischool.berkeley.edu>
2017-06-26 17:33:52 +0000

    commit

---
+++ w205/exercise_1/investigations/best_states/best_states.sql
@@ -0,0 +1,2 @@
+select * from (select format_number(avg(score),1) avg_score,format_number(stddev_pop(score),2) stddev, format_number(sum(score),1) agg_score, count(score) count_score, State from readmissions_transformed a, hospitals_transformed b where a.provider_id = b.provider_id group by State having sum(score) > 0 and count(score) >40) t order by avg_score asc, stddev desc  limit 20;
+
